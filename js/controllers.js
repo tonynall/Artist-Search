d54c98cd9a9d3dd428325d716fae1e15fe575cef
Tony Nall <Tony.Nall@capitalone.com>
2016-03-18 13:58:15 +0000

    Initial

---
+++ js/controllers.js
@@ -0,0 +1,31 @@
+var artistControllers = angular.module('artistControllers', ['ngAnimate']);
+
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
+
+$http.get('js/data2.json').success(function(data) {
+	$scope.artists = data;
+	$scope.artistOrder = 'tracks.album.name';
+  	
+	});
+}]);
+
+artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
+
+$http.get('js/data2.json').success(function(data) {
+	$scope.artists = data;
+	$scope.whichItem = $routeParams.itemId;
+
+	if($routeParams.itemId > 0) {
+		$scope.prevItem = Number($routeParams.itemId)-1;
+	} else {
+		$scope.prevItem = $scope.artists.length-1;
+	}
+  	
+	if($routeParams.itemId < $scope.artists.length-1) {
+		$scope.nextItem = Number($routeParams.itemId)+1;
+	} else {
+		$scope.nextItem = 0;
+	}
+
+	});
+}]);

ad841509cfdba99b5d3e060fee6eb1fed416b978
Tony Nall <Tony.Nall@capitalone.com>
2016-03-18 13:59:03 +0000

    reset

--- js/controllers.js
+++ js/controllers.js
@@ -2,16 +2,16 @@ var artistControllers = angular.module('artistControllers', ['ngAnimate']);
 
 artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http){
 
-$http.get('js/data2.json').success(function(data) {
+$http.get('js/data.json').success(function(data) {
 	$scope.artists = data;
-	$scope.artistOrder = 'tracks.album.name';
+	$scope.artistOrder = 'name';
   	
 	});
 }]);
 
 artistControllers.controller('DetailsController', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams){
 
-$http.get('js/data2.json').success(function(data) {
+$http.get('js/data.json').success(function(data) {
 	$scope.artists = data;
 	$scope.whichItem = $routeParams.itemId;
 
